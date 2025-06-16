6867581b6e197616f758bf21453b2e0e2feb8715
Aditya Prabhakar Parab <adityaparab37@gmail.com>
2025-06-16 18:02:02 +0530

    Sprinng data rest configs

--- 03-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/Dao/EmployeeRepository.java
+++ 03-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/Dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.luv2code.springboot.cruddemo.Dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     //That's it we are now using inbuilt functions of JPA repository
